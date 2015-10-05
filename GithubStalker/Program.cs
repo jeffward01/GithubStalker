41d23d8f2d60ee777552b638fb2a595d7f9cefbd
jeffward01 <jeffwardcoding@gmail.com>
2015-10-05 12:23:51 -0700

    working base model

--- GithubStalker/Program.cs
+++ GithubStalker/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+
 
 namespace GithubStalker
 {
@@ -10,6 +14,20 @@ namespace GithubStalker
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter your UserName: ");
+            string SearchUserName = Console.ReadLine();
+
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+
+                string json = webClient.DownloadString("https://api.github.com/users/" + SearchUserName);
+
+
+                Console.WriteLine(json);
+                Console.ReadLine();
+            }
+
         }
     }
 }
